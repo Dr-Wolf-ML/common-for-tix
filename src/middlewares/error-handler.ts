204e58306a2ce84436444f01c2b67cf787444cf0
Dr-Wolf-ML <wolfgangml@mac.com>
2021-06-09 14:49:17 +1000

    Automatic Update via script execution

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -3,16 +3,19 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  if (err instanceof CustomError) {
-    res.status(err.statusCode).send({ errors: err.serialiseErrors() });
-  }
+    if (err instanceof CustomError) {
+        res.status(err.statusCode).send({ errors: err.serialiseErrors() });
+    }
 
-  res.status(400).send({
-    errors: [{ message: err.message }],
-  });
+    //! not a CustomError ?! Let's log it out and inspect it...
+    console.error('Error not handled in CustomError: ', err);
+
+    res.status(400).send({
+        errors: [{ message: err.message }],
+    });
 };
