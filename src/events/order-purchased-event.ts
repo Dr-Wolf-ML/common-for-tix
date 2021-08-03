12643368b19f701fbff71b5091681a55ca3c416b
Dr-Wolf-ML <wolfgangml@mac.com>
2021-08-03 14:01:56 +1000

    Automatic Patch Update via script execution

--- src/events/order-purchased-event.ts
+++ src/events/order-purchased-event.ts
@@ -1,18 +1,9 @@
 import { Subjects } from './types/subjects';
-import { OrderStatus } from './types/order-status';
 
 export interface OrderPurchasedEvent {
     subject: Subjects.OrderPurchased;
     data: {
-        id: string;
-        version: number;
-        status: OrderStatus; //! YAGNI
-        userId: string;
-        expiresAt: string;
-        ticket: {
-            id: string;
-            price: number;
-            isPurchased: string; // contains the correponding orderId
-        };
+        orderId: string;
+        ticketId: string;
     };
 }
